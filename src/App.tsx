4d65d79d21ef3f42a4e1f9dae5439296d4d43a43
Sergio Romano <sergio.h.romano@gmail.com>
2023-08-27 10:22:35 -0300

    feat: current date calculated

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,12 @@ import './App.css';
 function App() {
   const [hideGrid, setHideGrid] = useState(false);
   const [hideTitle, setHideTitle] = useState(false);
+  const now = new Date().toLocaleDateString('pt-BR');
 
   return (
     <div>
       <Title
-        defaultTitle="Loteria Familiar - 22/11/22"
+        defaultTitle={`Loteria Familiar - ${now}`}
         hide={hideTitle}
         onSelected={() => setHideGrid((prev) => !prev)}
       />
